a6b2f296b073606b6fa1c740bf9ff6bf2f00526e
KDHwan <litmu@naver.com>
2024-03-06 16:55:06 +0900

    fix : Scene 구분
    DungeonScene(전투)
    TownScene(마을) 구분

---
+++ Assets/_Script/PlayerState.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+    public Character character;
+    public Equip[] equip;
+
+    
+}
