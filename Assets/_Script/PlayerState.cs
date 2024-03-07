3864d0b1cf8864b83b88a9ec4ee96667697b0175
ruiroll <hyunbean67@gmail.com>
2024-03-07 21:50:43 +0900

    feat : 착용관리 매서드 추가
    PlayerState 변경

--- Assets/_Script/PlayerState.cs
+++ Assets/_Script/PlayerState.cs
@@ -5,7 +5,25 @@ using UnityEngine;
 public class PlayerState : MonoBehaviour
 {
     public Character character;
-    public Equip[] equip;
+    public List<EquipItem> equip;
 
-    
+    public void EquipNewItem()
+    {
+        int len = equip.Count;
+        if (len < 3)
+        {
+            equip.Add(UIManager.instance.nowSelectedEquip);
+            UIManager.instance.nowSelectedEquip.isEquipped = true;
+        }
+        UIManager.instance.detailArea.ChangeDetailActivation(false);
+        UIManager.instance.nEquipItemSlot.FreshEquippedSlot();
+    }
+
+    public void UnEquipItem()
+    {
+        equip.Remove(UIManager.instance.nowSelectedEquip);
+        UIManager.instance.nowSelectedEquip.isEquipped = false;
+        UIManager.instance.detailArea.ChangeDetailActivation(false);
+        UIManager.instance.nEquipItemSlot.FreshEquippedSlot();
+    }
 }
