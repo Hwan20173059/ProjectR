4f0b7d4d73beb3ccbe380f62234862b7370ed8c2
ruiroll <hyunbean67@gmail.com>
2024-03-07 21:52:41 +0900

    Feat - 인벤토리 UI 및 기능 구현

---
+++ Assets/PSFolder/Inventory/EquipSlot.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using Unity.VisualScripting;
+
+public class EquipSlot : MonoBehaviour
+{
+    [SerializeField] private Image image;
+
+    private EquipItem _item;
+    public EquipItem item {
+        get { return _item; }
+        set {
+            _item = value;
+            if (_item != null)
+            {
+                image.sprite = item.data.itemSprite;
+                image.color = new Color(1, 1, 1, 1);
+            }
+            else
+            {
+                image.color = new Color(1, 1, 1, 0);
+            }
+        }
+    }
+
+    public void OnClickItem()
+    {
+        UIManager.instance.nowSelectedEquip = item;
+        UIManager.instance.detailArea.ChangeSelectedItem(item);
+    }
+}
---
+++ Assets/PSFolder/Inventory/InfiniteInventory.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfiniteInventory : Inventory
+{
+    [SerializeField] GameObject itemSlotPrefab;
+    [SerializeField] int maxSlots;
+    public int MaxSlots
+    {
+        get { return maxSlots; }
+        set { SetMaxSlots(value); }
+    }
+
+    protected override void Start()
+    {
+        SetMaxSlots(maxSlots);
+        base.Start();
+    }
+
+    private void SetMaxSlots(int value)
+    {
+        if(value <= 0)
+            maxSlots = 1;
+        else
+            maxSlots = value;
+
+        if (maxSlots <= slots.Count)
+        {
+            for(int i = maxSlots; i < slots.Count; i++)
+            {
+                Destroy(slots[i].transform.parent.gameObject);
+            }
+            int diff = slots.Count - maxSlots;
+            slots.RemoveRange(maxSlots, diff);
+        }
+        else if (maxSlots > slots.Count)
+        {
+            int diff = maxSlots - slots.Count;
+
+            for (int i = 0; i < diff; i++)
+            {
+                GameObject itemSlotGameObj = Instantiate(itemSlotPrefab);
+                itemSlotGameObj.transform.SetParent(slotParent, worldPositionStays: false);
+                slots.Add(itemSlotGameObj.GetComponentInChildren<EquipSlot>());
+            }
+        }
+    }
+}
