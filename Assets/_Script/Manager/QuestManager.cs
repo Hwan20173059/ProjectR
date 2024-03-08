97d1fba15a78470a2bc94eeab38bf945b2f3c3e3
KDHwan <litmu@naver.com>
2024-03-08 15:29:01 +0900

    fix : 추가 수정

--- Assets/_Script/Manager/QuestManager.cs
+++ Assets/_Script/Manager/QuestManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class QuestManager : Singleton<PlayerState>
+public class QuestManager : Singleton<QuestManager>
 {
     // Start is called before the first frame update
     void Start()
