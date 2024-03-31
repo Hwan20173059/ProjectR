0f321fed3a7c2be94e712c0d9e2d1fb64bd06306
KDHwan <litmu@naver.com>
2024-03-31 20:40:22 +0900

    fix 일부 코드 리팩토링

--- Assets/@KDH/DungeonManager.cs
+++ Assets/@KDH/DungeonManager.cs
@@ -75,39 +75,6 @@ public class DungeonManager : TileMapManager
         SceneManager.LoadScene("FieldScene");
     }
 
-    protected override void TileOn(int X, int Y)
-    {
-        if (X > -1 && X < 8 && Y > -1 && Y < 8 && TileStateCheck(X, Y, TileState.empty))
-            TileStateSetting(X, Y, TileState.canGO);
-        else if (X > -1 && X < 8 && Y > -1 && Y < 8 && TileStateCheck(X, Y, TileState.monster))
-            TileStateSetting(X, Y, TileState.canFight);
-        else if (X > -1 && X < 8 && Y > -1 && Y < 8 && TileStateCheck(X, Y, TileState.town))
-            TileStateSetting(X, Y, TileState.canTownEnter);
-        else if (X > -1 && X < 8 && Y > -1 && Y < 8 && TileStateCheck(X, Y, TileState.dungeon))
-            TileStateSetting(X, Y, TileState.canDungeonEnter);
-        else if (X > -1 && X < 8 && Y > -1 && Y < 8 && TileStateCheck(X, Y, TileState.chest))
-            TileStateSetting(X, Y, TileState.canOpenChest);
-    }
-
-    protected override void TileOff(int X, int Y)
-    {
-        if (X > -1 && X < 8 && Y > -1 && Y < 8 && TileStateCheck(X, Y, TileState.canGO))
-            TileStateSetting(X, Y, TileState.empty);
-        else if (X > -1 && X < 8 && Y > -1 && Y < 8 && TileStateCheck(X, Y, TileState.canFight))
-            TileStateSetting(X, Y, TileState.monster);
-        else if (X > -1 && X < 8 && Y > -1 && Y < 8 && TileStateCheck(X, Y, TileState.canTownEnter))
-            TileStateSetting(X, Y, TileState.town);
-        else if (X > -1 && X < 8 && Y > -1 && Y < 8 && TileStateCheck(X, Y, TileState.canDungeonEnter))
-            TileStateSetting(X, Y, TileState.dungeon);
-        else if (X > -1 && X < 8 && Y > -1 && Y < 8 && TileStateCheck(X, Y, TileState.canOpenChest))
-            TileStateSetting(X, Y, TileState.chest);
-    }
-
-    protected override bool FieldStateEmptyCheck(int X, int Y)
-    {
-        return X > -1 && X < 7 && Y > -1 && Y < 5 && field.tileRaw[Y].fieldTiles[X].tileState == TileState.empty;
-    }
-
     protected void ClearDungeon()
     {
         dungeonClearUI.SetActive(true);
