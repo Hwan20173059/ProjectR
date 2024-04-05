3396b1c60974d2f341911fdc507dd47166349235
minsang95 <154496356+minsang95@users.noreply.github.com>
2024-04-05 11:52:52 +0900

    fix : 버프핸들러 재귀함수 수정

--- Assets/@JMS/Scripts/Unit/Character/CharacterBuffHandler.cs
+++ Assets/@JMS/Scripts/Unit/Character/CharacterBuffHandler.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class CharacterBuffHandler
 {
     public int atkBuff;
-    public int atkDuration { get { return atkDuration; } set { atkDuration = value; if (atkDuration <= 0) atkBuff = 0; } }
-
+    public int atkDuration;
     public int speedBuff;
-    public int speedDuration { get { return speedDuration; } set { speedDuration = value; if (speedDuration <= 0) speedBuff = 0; } }
+    public int speedDuration;
 
     public void BuffDurationReduce()
     {
-        if (atkDuration > 0) { atkDuration--; }
-        if (speedDuration > 0) { speedDuration--; }
+        if (atkDuration > 0)
+        {
+            --atkDuration;
+            if(atkDuration <= 0) { atkBuff = 0; }
+        }
+        if (speedDuration > 0)
+        {
+            --speedDuration;
+            if (speedDuration <= 0) { speedBuff = 0; }
+        }
     }
 }
