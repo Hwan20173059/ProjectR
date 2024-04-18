9c95b31bb58af62dd8e8e06916e91668cd2fd0e3
KDHwan <litmu@naver.com>
2024-04-18 09:27:36 +0900

    feat 해상도 고정 기능 추가

---
+++ Assets/_Script/Player/FixedCamera.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedCamera : MonoBehaviour
+{
+    private void Start()
+    {
+        SetResolution();
+    }
+
+    public void SetResolution()
+    {
+        int setWidth = 2340;
+        int setHeight = 1080;
+
+        int deviceWidth = Screen.width;
+        int deviceHeight = Screen.height;
+
+        Screen.SetResolution(setWidth, (int)(((float)deviceHeight / deviceWidth) * setWidth), true);
+
+        if ((float)setWidth / setHeight < (float)deviceWidth / deviceHeight)
+        {
+            float newWidth = ((float)setWidth / setHeight) / ((float)deviceWidth / deviceHeight);
+            Camera.main.rect = new Rect((1f - newWidth) / 2f, 0f, newWidth, 1f);
+        }
+        else 
+        {
+            float newHeight = ((float)deviceWidth / deviceHeight) / ((float)setWidth / setHeight);
+            Camera.main.rect = new Rect(0f, (1f - newHeight) / 2f, 1f, newHeight);
+        }
+    }
+}
